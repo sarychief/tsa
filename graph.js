0e5c571c2491e169e7cd42fa76e2366a62a55421
sarychief <92107574+sarychief@users.noreply.github.com>
2023-11-07 15:18:48 +0300

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,68 +1,69 @@
-d3.json("Time_series_map.json").then(function(data) {
-    var svg = d3.select("#myGraph"),
-        width = +svg.attr("width"),
-        height = +svg.attr("height");
+d3.json("Time_series_map.canvas").then(function(data) {
+   var svg = d3.select("#myGraph"),
+       width = +svg.attr("width"),
+       height = +svg.attr("height");
 
-    var simulation = d3.forceSimulation()
-        .force("link", d3.forceLink().id(function(d) { return d.id; }))
-        .force("charge", d3.forceManyBody())
-        .force("center", d3.forceCenter(width / 2, height / 2));
-    
-    var link = svg.append("g")
-        .attr("class", "links")
-        .selectAll("line")
-        .data(data.edges)
-        .enter().append("line");
+   var simulation = d3.forceSimulation()
+       .force("link", d3.forceLink().id(function(d) { return d.id; }))
+       .force("charge", d3.forceManyBody())
+       .force("center", d3.forceCenter(width / 2, height / 2));
+   
+   var link = svg.append("g")
+       .attr("class", "links")
+       .selectAll("line")
+       .data(data.edges)
+       .enter().append("line");
 
-    var node = svg.append("g")
-        .attr("class", "nodes")
-        .selectAll("circle")
-        .data(data.nodes)
-        .enter().append("circle")
-        .attr("r", 5)
-        .call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended));
-    
-    node.append("title")
-        .text(function(d) { return d.id; });
+   var node = svg.append("g")
+       .attr("class", "nodes")
+       .selectAll("rect") // change this to "rect" to match the structure of your nodes
+       .data(data.nodes)
+       .enter().append("rect") // change this to "rect" to match the structure of your nodes
+       .attr("width", function(d) { return d.width; }) // set the width of the rectangles
+       .attr("height", function(d) { return d.height; }) // set the height of the rectangles
+       .call(d3.drag()
+           .on("start", dragstarted)
+           .on("drag", dragged)
+           .on("end", dragended));
+   
+   node.append("title")
+       .text(function(d) { return d.id; });
 
-    simulation
-        .nodes(data.nodes)
-        .on("tick", ticked);
+   simulation
+       .nodes(data.nodes)
+       .on("tick", ticked);
 
-    simulation.force("link")
-        .links(data.edges);
+   simulation.force("link")
+       .links(data.edges);
 
-    function dragstarted(d) {
-        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-        d.fx = d.x;
-        d.fy = d.y;
-    }
+   function dragstarted(d) {
+       if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+       d.fx = d.x;
+       d.fy = d.y;
+   }
 
-    function dragged(d) {
-        d.fx = d3.event.x;
-        d.fy = d3.event.y;
-    }
+   function dragged(d) {
+       d.fx = d3.event.x;
+       d.fy = d3.event.y;
+   }
 
-    function dragended(d) {
-        if (!d3.event.active) simulation.alphaTarget(0);
-        d.fx = null;
-        d.fy = null;
-    }
+   function dragended(d) {
+       if (!d3.event.active) simulation.alphaTarget(0);
+       d.fx = null;
+       d.fy = null;
+   }
 
-    function ticked() {
-        link
-            .attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
+   function ticked() {
+       link
+           .attr("x1", function(d) { return d.source.x; })
+           .attr("y1", function(d) { return d.source.y; })
+           .attr("x2", function(d) { return d.target.x; })
+           .attr("y2", function(d) { return d.target.y; });
 
-        node
-            .attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
-    }
+       node
+           .attr("x", function(d) { return d.x; })
+           .attr("y", function(d) { return d.y; });
+   }
 }).catch(function(error) {
-    console.log(error);
+   console.log(error);
 });
