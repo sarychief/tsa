f73e5ffc9bf9b3b2e18b61e9039ce3f65e0f260d
sarychief <92107574+sarychief@users.noreply.github.com>
2023-11-09 14:32:10 +0300

    Update graph.js

--- graph.js
+++ graph.js
@@ -1,33 +1,34 @@
 // Загрузите и проанализируйте данные графа из файла .canvas
 d3.json('Time_series_map.canvas').then(function(graphData) {
 
-    // Отображение узлов графа
-    let nodes = d3.select('#myGraph').selectAll('circle')
-        .data(graphData.nodes)
-        .enter()
-        .append('circle');
+    // Создайте новый экземпляр sigma
+    var s = new sigma('myGraph');
 
-    nodes.attr('cx', function(d) { return d.x; })
-        .attr('cy', function(d) { return d.y; })
-        .attr('r', 20)
-        .style('fill', 'blue');
+    // Добавьте узлы в граф sigma
+    graphData.nodes.forEach(function(node, index) {
+        s.graph.addNode({
+            id: node.id,
+            label: node.text,
+            x: node.x,
+            y: node.y,
+            size: 1,
+            color: 'blue'
+        });
+    });
 
-    // Отображение связей между узлами
-    let edges = d3.select('#myGraph').selectAll('line')
-        .data(graphData.edges)
-        .enter()
-        .append('line');
+    // Добавьте связи в граф sigma
+    graphData.edges.forEach(function(edge, index) {
+        s.graph.addEdge({
+            id: 'e' + index,
+            source: edge.fromNode,
+            target: edge.toNode,
+            color: 'black'
+        });
+    });
 
-    edges.attr('x1', function(d) { return getNodeById(d.fromNode).x; })
-        .attr('y1', function(d) { return getNodeById(d.fromNode).y; })
-        .attr('x2', function(d) { return getNodeById(d.toNode).x; })
-        .attr('y2', function(d) { return getNodeById(d.toNode).y; })
-        .style('stroke', 'black')
-        .style('stroke-width', 2);
+    // Обновите граф, чтобы отобразить добавленные узлы и связи
+    s.refresh();
 
-    function getNodeById(id) {
-        return graphData.nodes.find(node => node.id === id);
-    }
 }).catch(function(error) {
     console.log('Ошибка при загрузке или анализе данных графа: ', error);
 });
