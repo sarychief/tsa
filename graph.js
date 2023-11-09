603cea40f869c3cfb884c9ed85c8e166afba168b
sarychief <92107574+sarychief@users.noreply.github.com>
2023-11-09 23:32:45 +0300

    Update graph.js

--- graph.js
+++ graph.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
             // Добавьте узлы в граф sigma
             graphData.nodes.forEach(function(node, index) {
                 // Извлекаем название файла из ссылки, если есть свойство file, в противном случае используем свойство text
-                var fileName = node.file ? node.file.match(/topics\/(.+?)\.md/)[1] : node.text;
+                var fileName = node.file ? node.file.replace(/^.*[\\\/]/, '').replace('.md', '') : node.text;
             
                 s.graph.addNode({
                     id: node.id,
